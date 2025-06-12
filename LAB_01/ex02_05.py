2386dde653ff7f30029416f68b89e6b01231f581
Trần Sỷ Sơn Lâm <transysonlam@gmail.com>
2025-06-12 10:54:39 +0700

    Delete LAB_01 directory

--- LAB_01/ex02_05.py
+++
@@ -1,9 +0,0 @@
-so_gio_lam = float(input("Nhập số giờ làm mỗi tuần: "))
-luong_gio = float(input("Nhập số tiền công mỗi giờ làm: "))
-
-gio_tieu_chuan = 44
-gio_vuot_chuan = max(0, so_gio_lam - gio_tieu_chuan)
-
-thuc_linh = gio_tieu_chuan * luong_gio +gio_vuot_chuan * luong_gio * 1.5
-
-print(f"Số tiền thực lĩnh của nhân viên: {thuc_linh}")
